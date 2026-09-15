9caa6d691c7529dabcd3db9966bcb51e8dad609e
sujin <isujin171@gmail.com>
2026-09-15 17:51:42 +0900

    이름 오타 수정

--- src/App.jsx
+++ src/App.jsx
@@ -104,7 +104,7 @@ const students = [
     topic: "",
   },
   {
-    name: "Sujin Lee",
+    name: "Sujin ee",
     level: "undergraduate",
     degree: "Undergraduate Student",
     email: "sujin7032@hanyang.ac.kr",
